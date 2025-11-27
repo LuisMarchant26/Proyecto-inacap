89497d36242ef06d1b101839a316d024feeb89e3
LuisMarchant26 <luis.marchant09@inacapmail.cl>
2025-11-26 21:29:10 -0300

    Preparando para deploy: Gunicorn + Whitenoise

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-=n81@9^g07rgu&ppv%5pe_l^ne!(osd_hzimk3=krvr=ut59=-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+TATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
